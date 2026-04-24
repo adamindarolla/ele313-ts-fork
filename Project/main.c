0800cd318f4f2e30845b167d6a9a4275efd31ff6
tomseipp <thisistomseipp@gmail.com>
2026-04-24 11:08:03 +0100

    initial following ir behaviour

--- Project/main.c
+++ Project/main.c
@@ -1,3 +1,7 @@
+// TASK 2
+
+
+// Adam was actually here 22/4/26
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,57 +18,17 @@
 #include "stdio.h"
 #include "serial_comm.h"
 #include "selector.h"
+#include "sensors/VL53L0X/VL53L0X.h"
 
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-// declare wall following mode
-void follow_wall() {
-			
-			if (get_prox(prefSide)<minirread){
-				//turn towards wall
-				float wheelratio = 0.9;
-			} else if ((get_prox(prefSide)>maxirread && get_prox(offSide)<maxirread) || (get_prox(prefCorner)>maxirread && get_prox(offCorner)<maxirread)){
-				//turn away from preffered wall
-				float wheelratio = 1.1;
-			} else if ((get_prox(offSide)>maxirread && get_prox(prefSide)<maxirread) || (get_prox(offCorner)>maxirread && get_prox(prefCorner)<maxirread)){
-				// turn away from offside wall
-				float wheelratio = 0.9;
-			} else if ((get_prox(prefSide)>maxirread && get_prox(offSide)>maxirread) || (get_prox(prefCorner)>maxirread && get_prox(offCorner)>maxirread)){
-				// if robot is too close to both walls, turn around 180 ish degrees
-				left_motor_set_speed(-500 * followSide);
-				right_motor_set_speed(500 * followSide); 
-				chThdSleepMilliseconds(1000); // 1 second is abritary amount of time
-			} else if (get_prox(prefFront)>maxirread || (get_prox(offFront)>maxirread)){
-                // if wall is infront of robot, turn away from desired side
-				left_motor_set_speed(-500 * followSide);
-				right_motor_set_speed(500 * followSide); 
-				chThdSleepMilliseconds(500); // 1 second is abritary amount of time
-            }
-
-			// sets wheel speeds based case seen before
-			if (followsSide ==-1){
-				if (wheelRatio<1){
-					left_motor_set_speed(1000 * wheelRatio);
-					right_motor_set_speed(1000); 
-				}else{
-					left_motor_set_speed(1000);
-					right_motor_set_speed(1000 / wheelRatio); 
-				}	
-			} else if (followside ==1){
-				if (wheelRatio<1){
-					left_motor_set_speed(1000);
-					right_motor_set_speed(1000 * wheelRatio); 
-				} else {
-					left_motor_set_speed(1000 / wheelRatio);
-					right_motor_set_speed(1000); 
-				}
-			}
-		}
 
 
-int main(void){
+// Initialising stuff
+
+int main(void){ 
     halInit();
     chSysInit();
     mpu_init();
@@ -77,7 +41,7 @@ int main(void){
     //LED
     clear_leds();
     spi_comm_start();
-	rgb_led_name_t allRGBs[] = {LED2, LED4, LED6, LED8};
+	
 
     //Motors
     motors_init();
@@ -85,174 +49,148 @@ int main(void){
 	//Bluetooth
 	serial_start();
 
+    
+    // variable declarations
+	int front_right;
+	int front_left;
+	int corner_right;
+	int corner_left;
+	int side_right;
+	int side_left;
+	int wheelMult;
+	int strongest;
+	int strongest_ir;
 
-// our stuff
-    bool wallsexplored=0;
-    int SensorValue[8]; // array for sensor values
-	int minirread = 300;
-	int maxirread = 1000;
-	float wheelRatio = 1; // if less than 1, preffered side wheel spins slower therefore turns towards preffered wall
-	int32_t maxwheelsteps = 25000; // rough guess of number of steps around box
-	
-	enum RobotState { NOWALL, FOUNDWALL, EXPLORING };
-	enum RobotState currentState = NOWALL;
+	int minir = 100;
+	int threshold = 1000;
+	int selecta;
+	int tof;
+	int tof_thresh=100;
 
-	int followSide = 0; // 1 for Right, -1 for Left
-	int threshold = 150; // Distance to trigger "Wall Found"
-	int maxVal = 0;
-	int strongestSensor;
-	
-    // Task 1
-    // check if its next to a wall
-	// if yes - go forwards
-	// if no - enter wall following mode
-	// decide which side the wall is on
+	while (1){ // infinite Main Loop!
+		while (get_selector() < 11){ // when selector >=11 robot wont move, if it was over 11 wheelspeed would be too high
+			wheelMult = get_selector()
+			front_right = get_calibrated_prox(0);
+			front_left = get_calibrated_prox(7);
+			corner_right = get_calibrated_prox(1);
+			corner_left = get_calibrated_prox(6);
+			side_right = get_calibrated_prox(2);
+			side_left = get_calibrated_prox(5);
 
-	//infinite goon loop Ithis is for whole thing)
-	
-	while(get_selector()==1) {
-		
-    //check if any walls nearby and which is strongest sensor
-	int maxVal = 0; 
-		
-	for (int i = 0; i < 8; i++) {
-    int currentVal = get_prox(i);
-    if (currentVal > maxVal) {
-        maxVal = currentVal;
-        strongestSensor = i;
-    }
-	}
-		if (currentState == NOWALL) {
-			if (maxVal > threshold) {
-				char str1[100];
-				int str1_length;
-				str1_length = sprintf(str1, "Wall Detected\n");
-				e_send_uart1_char(str1, str1_length);
+			strongest = 0;
 
-				
-			// this is when it has detected a wall
-			// finding which side of puck max sensor reading is on
-    switch (strongestSensor) {
-        case 0
-			followSide = 1; //right
-			currentState = FOUNDWALL;
-        case 1
-			followSide = 1;
-			currentState = FOUNDWALL;
-        case 2
-			followSide = 1;
-			currentState = FOUNDWALL;
-        case 3
-			followSide = 1;
-			currentState = FOUNDWALL;
-		case 4
-			followSide = -1; //left
-			currentState = FOUNDWALL;
-        case 5
-			followSide = -1;
-			currentState = FOUNDWALL;
-        case 6
-			followSide = -1;
-			currentState = FOUNDWALL;
-        case 7
-			followSide = -1;
-			currentState = FOUNDWALL;
-	}
-				// sets which sensors are preffered side and offside
-		if (followSide == 1){
-			int prefFront = 0;
-			int prefCorner = 1;
-			int prefSide = 2;
-			int prefBack = 3;
-			int offFront = 7;
-			int offCorner = 6;
-			int offSide = 5;
-			int offBack = 4;
-		} else {
-			int prefFront = 7;
-			int prefCorner = 6;
-			int prefSide = 5;
-			int prefBack = 4;
-			int offFront = 0;
-			int offCorner = 1;
-			int offSide = 2;
-			int offBack = 3;
-		}
-			}else {
-				// no wall detected - set motor speed to arbitrary value
-				left_motor_set_speed(1000);
-				right_motor_set_speed(1000); 
-    		}	
-	}
-// this is where we need the bit for wall following
-		if (currentstate==FOUNDWALL && wallsexplored==0 ) {
-			left_motor_set_pos(0);
-			right_motor_set_pos(0);
-			while (left_motor_get_pos()< maxwheelsteps && right_motor_get_pos()< maxwheelsteps){
-				follow_wall()
-				
-			}
-			bool wallsexplored = 1;
-			currentState = EXPLORING;
-				char str2[100];
-				int str2_length;
-				str2_length = sprintf(str2, "Entering Explore Mode\n");
-				e_send_uart1_char(str2, str2_length);
-			
-		}
-		
-
-		// exploration mode
-	if (wallsexplored) {
-	// check if wall is reached
-		if (SensorValue[0] > threshold || SensorValue[7] > threshold || SensorValue[1] > threshold || SensorValue[2] > threshold || SensorValue[5] > threshold ||SensorValue[6] > threshold || ) {
-			currentState = FOUNDWALL;
-			left_motor_set_pos(0);
-			right_motor_set_pos(0);
-			while (left_motor_get_pos()<maxwheelsteps2 && right_motor_get_pos()<maxwheelsteps2){
-				follow_wall()
-					}
-
-				// Here we need it to just do the wall following function for a small amount of time - in case there is any obstacle
-				// or if it has not rotated exactly 90 degrees
-				// then it turns 90 degrees
-
-			right_motor_set_speed(500*followSide);
-			left_motor_set_speed(-500*followSide);
-			delay_ms(500);
-			currentState = EXPLORING;
-			followSide= -followSide;
-
-						}
-			else
-			left_motor_set_speed(1000);
-			left_motor_set_speed(1000);
-					}
+			//find max value
+			for (i=0; i<8; i++){
+				if (get_calibrated_prox(i)>get_calibrated_prox(strongest)){
+					strongest = i;
 				}
 			}
-		}
 
-	if currentState = NOWALL {
-		for (int i = 0; i <= 4; i++) 
-		set_rgb_led(allRGBs[i], 10 , 0 , 0 );
-	}
-	else if currentState = FOUNDWALL {
-		for (int i = 0; i <= 4; i++) 
-		set_rgb_led(allRGBs[i], 0 , 10 , 0 );
-	}
-	else if currentState = EXPLORING {
-		for (int i = 0; i <= 4; i++) 
-		set_rgb_led(allRGBs[i], 0 , 0 , 10);
-	}
-	
-	
-		
-	
-	}
-		
+			strongest_ir = get_calibrated_prox(strongest);
 
-		
-	}
+			
+			// if it can see the object
+			if (strongest_ir>minir){
+				// if it is close enough already, stop then turn
+				if (strongest_ir>threshold){ // only runs when robot is super close
+					switch (strongest){
+						// turn until maximum is at front
+						case 0:// infront therefore do nothing
+							left_motor_set_speed(0);
+							right_motor_set_speed(0);
+						break;
+						case 1:
+						case 2:
+						case 3:
+							while (strongest!=0 ||strongest!=7){ // runs while neither of the front sensors are strongest
+								left_motor_set_speed(500); // on right therfore spin right
+								right_motor_set_speed(-500);
+								//find strongest sensor each loop of while
+								for (i=0; i<8; i++){
+									if (get_calibrated_prox(i)>get_calibrated_prox(strongest)){
+									strongest = i;
+									}
+								}
+							}
+							left_motor_set_speed(0); // once object is in front, stop spinning
+							right_motor_set_speed(0);
+						break;
+						case 4:
+						case 5:
+						case 6:
+						//on left spin left
+							while (strongest!=0 ||strongest!=7){
+								left_motor_set_speed(-500);
+								right_motor_set_speed(500);
+								//find max value
+								for (i=0; i<8; i++){
+									if (get_calibrated_prox(i)>get_calibrated_prox(strongest)){
+									strongest = i;
+									}
+								}
+							}
+							left_motor_set_speed(0); // once object is in front, stop spinning
+							right_motor_set_speed(0);
+						break;
+						case 7: // infront therfore do nothing
+							left_motor_set_speed(0);
+							right_motor_set_speed(0);
+						break;
+					}
+				
+				}else{ // only runs when at middle distance (ir can see object but not touching)
+					switch(strongest){
+						case 0:
+							left_motor_set_speed(700);
+							right_motor_set_speed(700);
+							break;
+						case 1:
+							left_motor_set_speed(700);
+							right_motor_set_speed(500);
+							break;
+						case 2:
+							left_motor_set_speed(600);
+							right_motor_set_speed(100);
+							break;
+						case 3:
+							left_motor_set_speed(500);
+							right_motor_set_speed(-500);
+							break;
+						case 4:
+							left_motor_set_speed(-500);
+							right_motor_set_speed(500);
+							break;
+						case 5:
+							left_motor_set_speed(100);
+							right_motor_set_speed(600);
+							break;
+						case 6:
+							left_motor_set_speed(500);
+							right_motor_set_speed(700);
+							break;
+						case 7:
+							left_motor_set_speed(700);
+							right_motor_set_speed(700);
+							break;
+					}
+				}
+			} else{	
+			//else
+				// find the object by spinning or whatever tof sensor
+
+				// get tof
+				//get selcta
+				//while tof is greater than threshold, spin
+				// once tof can see object, go forwards
+			}
+
+			
+		} //end of selector loop
+	} // end of infinite loop
+	
+   
+} // end of main
+
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
